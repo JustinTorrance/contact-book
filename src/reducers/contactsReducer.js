fca343ad891f387df21b7a1e985db81b158b4ac6
Justin Torrance <justin.torrance91@gmail.com>
2019-10-16 14:07:57 -0600

    Delete contact from store when user clicks delete button on contact

--- src/reducers/contactsReducer.js
+++ src/reducers/contactsReducer.js
@@ -1,7 +1,11 @@
+import shortid from 'shortid'
+
 export const contactsReducer = (state = [], action) => {
   switch(action.type) {
     case 'ADD_CONTACT':
-      return [...state, action.contact]
+      return [...state, {...action.contact, id: shortid.generate()}]
+    case 'DELETE_CONTACT':
+      return state.filter(contact => (contact.id !== action.id))
     default:
       return state
   }
