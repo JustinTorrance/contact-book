205b620d9c2abaf72a8873fa87caedea3ad4efed
Justin Torrance <justin.torrance91@gmail.com>
2019-10-16 11:11:14 -0600

    Convert ContactForm to class-based component. Create controlled form

--- src/containers/ContactForm/ContactForm.js
+++ src/containers/ContactForm/ContactForm.js
@@ -1,11 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react'
 
-function ContactForm() {
-  return (
-    <div className="ContactForm">
-      <p>ContactForm</p>
-    </div>
-  );
+export class ContactForm extends Component {
+  constructor() {
+    super()
+    this.state = {
+      firstName: '',
+      lastName: '',
+      phone: '',
+      email: ''
+    }
+  }
+
+  handleChange = (e) => {
+    const { name, value } = e.target
+    this.setState({ [name]: value})
+  }
+
+  render() {
+
+    const { firstName, lastName, phone, email } = this.state
+
+    return(
+      <form>
+        <p>Create New Contact</p>
+        <input 
+          type="text"
+          value={firstName}
+          name='firstName'
+          placeholder='First name'
+          onChange={this.handleChange}
+        />
+        <input 
+          type="text"
+          value={lastName}
+          name='lastName'
+          placeholder='Last name'
+          onChange={this.handleChange}
+        />
+        <input 
+          type="number"
+          value={phone}
+          name='phone'
+          placeholder='Phone'
+          onChange={this.handleChange}
+        />
+        <input 
+          type="text"
+          value={email}
+          name='email'
+          placeholder='Email'
+          onChange={this.handleChange}
+        />
+      </form>
+    )
+  }
 }
-
-export default ContactForm;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
 import './index.css';
 import App from './containers/App/App';
-import * as serviceWorker from './serviceWorker';
+// import rootReducer from './reducers';
+// import { Provider } from 'react-redux';
+// import { createStore, applyMiddleware } from 'redux';
+// import { composeWithDevTools } from 'redux-devtools-extension';
+// import thunk from 'redux-thunk';
+
+// const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>, document.getElementById('root'));
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
